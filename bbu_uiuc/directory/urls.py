cf031d3abc46fb3bea90ac6183c565fe7348be6b
tairesu <tyresecook25@yahoo.com>
2026-03-24 21:44:40 -0500

    Setup app level urls

---
+++ bbu_uiuc/directory/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+"""
+URL configuration for directory app.
+
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from directory.views import (
+    LandingPageView
+)
+urlpatterns = [
+    path('', LandingPageView.as_view(), name="home_urlpattern")
+]
+
+
