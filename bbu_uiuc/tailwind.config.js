edbee67c49be707c52e8560bc5678326e7a8ef8b
tairesu <tyresecook25@yahoo.com>
2026-03-30 14:41:26 -0500

    moved tailwind colors and spacing into extend property

--- bbu_uiuc/tailwind.config.js
+++ bbu_uiuc/tailwind.config.js
@@ -2,29 +2,30 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    colors: {
-      'greninja': '#0c73ae',
-      'grey': '#f4f4f4'
+    extend: {
+      colors: {
+        'greninja': '#0c73ae',
+        'grey': '#f4f4f4'
+      },
+      spacing: {
+        '2xs': `${(1/1.23)/1.23/1.23}rem`,
+        'xs': `${(1/1.23)/1.23}rem`,
+        'sm': `${1/1.23}rem`,
+        'base': '1rem',
+        'lg': `1.23rem`,
+        'xl': `${1.23^2}rem`,
+        '2xl': `${1.23^3}rem`,
+        '3xl': `${1.23^4}rem`,
+        '4xl': `${1.23^5}rem`,
+        '5xl': `${1.23^6}rem`,
+        '6xl': `${1.23^7}rem`,
+        '7xl': `${1.23^8}rem`,
+        '8xl': `${1.23^9}rem`,
+        '9xl': `${1.23^10}rem`,
+        '10xl': `${1.23^11}rem`,
+        '50vh': '50vh',
+      }
     },
-    extend: {},
-    spacing: {
-      '2xs': `${(1/1.23)/1.23/1.23}rem`,
-      'xs': `${(1/1.23)/1.23}rem`,
-      'sm': `${1/1.23}rem`,
-      'base': '1rem',
-      'lg': `1.23rem`,
-      'xl': `${1.23^2}rem`,
-      '2xl': `${1.23^3}rem`,
-      '3xl': `${1.23^4}rem`,
-      '4xl': `${1.23^5}rem`,
-      '5xl': `${1.23^6}rem`,
-      '6xl': `${1.23^7}rem`,
-      '7xl': `${1.23^8}rem`,
-      '8xl': `${1.23^9}rem`,
-      '9xl': `${1.23^10}rem`,
-      '10xl': `${1.23^11}rem`,
-      '50vh': '50vh',
-    }
   },
   plugins: [],
 }
