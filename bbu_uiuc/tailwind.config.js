befea77b14561bde4d9b911d8b9e21465bdddeec
tairesu <tyresecook25@yahoo.com>
2026-03-30 19:34:05 -0500

    add the teal and orange to tailwind conf

--- bbu_uiuc/tailwind.config.js
+++ bbu_uiuc/tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'greninja': '#0c73ae',
-        'grey': '#f4f4f4'
+        'greninja': '#2ab6ca',
+        'grey': '#f4f4f4',
+        'orange': '#e2642f'
       },
       spacing: {
         '2xs': `${(1/1.23)/1.23/1.23}rem`,
@@ -13,16 +14,16 @@ module.exports = {
         'sm': `${1/1.23}rem`,
         'base': '1rem',
         'lg': `1.23rem`,
-        'xl': `${1.23^2}rem`,
-        '2xl': `${1.23^3}rem`,
-        '3xl': `${1.23^4}rem`,
-        '4xl': `${1.23^5}rem`,
-        '5xl': `${1.23^6}rem`,
-        '6xl': `${1.23^7}rem`,
-        '7xl': `${1.23^8}rem`,
-        '8xl': `${1.23^9}rem`,
-        '9xl': `${1.23^10}rem`,
-        '10xl': `${1.23^11}rem`,
+        'xl': `${1.23*2}rem`,
+        '2xl': `${1.23*3}rem`,
+        '3xl': `${1.23*4}rem`,
+        '4xl': `${1.23*5}rem`,
+        '5xl': `${1.23*6}rem`,
+        '6xl': `${1.23*7}rem`,
+        '7xl': `${1.23*8}rem`,
+        '8xl': `${1.23*9}rem`,
+        '9xl': `${1.23*10}rem`,
+        '10xl': `${1.23*11}rem`,
         '50vh': '50vh',
       }
     },
