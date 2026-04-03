4c2887d4e84c1f055e73611611e041bc38e9580e
tairesu <tyresecook25@yahoo.com>
2026-04-02 20:07:19 -0500

    init `utils.plugins.CategoryContextMixin`

---
+++ bbu_uiuc/bbu_uiuc/utils/plugins.py
@@ -0,0 +1,12 @@
+from django.views.generic.base import ContextMixin
+from directory.models import (
+    Category
+)
+
+
+# Provides List of Categories to Views
+class CategoryContextMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        return context
