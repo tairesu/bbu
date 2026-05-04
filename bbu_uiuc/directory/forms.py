887b99d4335ba30beb966ca4bfbfc1539177e842
tairesu <tyresecook25@yahoo.com>
2026-05-03 19:24:00 -0500

    add: phone field to newbusinessform

--- bbu_uiuc/directory/forms.py
+++ bbu_uiuc/directory/forms.py
@@ -14,6 +14,7 @@ class NewBusinessForm(forms.ModelForm):
             'description',
             'headline',
             'category',
+            'phone',
             'website',
             'instagram',
             'logo',
